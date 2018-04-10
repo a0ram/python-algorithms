b7d053af0c90bfd3a347960fb8c26e28185199fd
Alvaro Ramirez <alv.rsl@outlook.com>
2018-04-09 23:44:58 -0500

    Quick sort

---
+++ quick_sort.py
@@ -0,0 +1,17 @@
+# Name: Quick sort
+# Running Time: O(nlogn)
+
+def quicksort(array):
+    # base case, already "sorted"
+    if len(array) < 2:
+        return array
+    else:
+        # recursive case
+        pivot = array[0]
+        # sub-array of all the elements less than the pivot
+        lesser_elements = [i for i in array[1:] if i <= pivot]
+        # sub-array of all the elements greater than the pivot
+        greater_elements = [i for i in array[1:] if i >pivot]
+        return quicksort(lesser_elements) + [pivot] + quicksort(greater_elements)
+
+print(quicksort([10, 5, 2, 3, 1, 8, 13]))
--- selection_sort.py
+++ selection_sort.py
@@ -1,22 +1,22 @@
 # Name: Selection Sort
 # Running Time: O(n^2)
 
-def find_smallest(arr):
-    """Finds the smallest element of an array and returns its index"""
-    smallest = arr[0]
+def find_smallest(array):
+    """Finds the smallest element of an arrayay and returns its index"""
+    smallest = array[0]
     smallest_index = 0
-    for i in range(1, len(arr)):
-        if arr[i] < smallest:
-            smallest = arr[i]
+    for i in range(1, len(array)):
+        if array[i] < smallest:
+            smallest = array[i]
             smallest_index = i
     return smallest_index
 
-def selection_sort(arr):
-    """Sorts an array by finding its smallest or biggest element"""
-    new_arr = []
-    for i in range(len(arr)):
-        smallest = find_smallest(arr)
-        new_arr.append(arr.pop(smallest))
-    return new_arr
+def selection_sort(array):
+    """Sorts an arrayay by finding its smallest or biggest element"""
+    new_array = []
+    for i in range(len(array)):
+        smallest = find_smallest(array)
+        new_array.append(array.pop(smallest))
+    return new_array
 
 print(selection_sort([5, 3, 6, 2, 10]))
